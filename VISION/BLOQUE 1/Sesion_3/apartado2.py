85d7fee36a1bbd4e8d3a4378c78b2167e893c2eb
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-09-21 16:57:49 +0200

    Fin ejercicio 2 practica 2

--- VISION/BLOQUE 1/Sesion_3/apartado2.py
+++ VISION/BLOQUE 1/Sesion_3/apartado2.py
@@ -1,15 +1,44 @@
-import cv2 as cv
+import cv2
 
-cap = cv.VideoCapture(0)
+video = "movimiento.mp4"
+cap   = cv2.VideoCapture(video)
+
+copias_rgb = []
+def extraccion_canales(imagen):
+    blue = imagen.copy()
+    # Extraigo el canal azúl
+    blue[:, :, 1] = 0
+    blue[:, :, 2] = 0
+
+    green = imagen.copy()
+    # Extraigo el canal verde
+    green[:, :, 0] = 0
+    green[:, :, 2] = 0
+
+    red = imagen.copy()
+    # Extraigo el canal rojo
+    red[:, :, 0] = 0
+    red[:, :, 1] = 0
+    return blue, green, red
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Display the resulting frame
-    cv.imshow('frame',gray)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        copia_frame = frame.copy()
+        copias_rgb = extraccion_canales(copia_frame)
+        cv2.imwrite("Extraccion_prueba.png",copias_rgb[2])
+        concatenacion_horizontal_1 = cv2.hconcat([copia_frame,copias_rgb[0]])
+        concatenacion_horizontal_2 = cv2.hconcat([copias_rgb[1],copias_rgb[2]])
+        concatenacion_final        = cv2.vconcat([concatenacion_horizontal_1,concatenacion_horizontal_2])
+        cv2.imwrite("Extraccion.png",concatenacion_final)
         break
 # When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
+
+
