ef68a8628da7b874e34f7cdcb84c888600666006
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-09-26 20:59:25 +0200

    Practica 4

---
+++ VISION/BLOQUE 1/Sesion_4/apartado1.py
@@ -0,0 +1,89 @@
+# coding: utf-8
+import cv2
+
+thickness = 2
+
+#Función para atender los eventos del ratón
+def EventoRaton(evento, x, y, flags,datos):
+    global color
+    global coordenada_inicial
+    global thickness
+    if evento == cv2.EVENT_MOUSEMOVE:
+    # Este evento se genera cada vez que el ratón se mueve por encima de la
+    # ventana. Se pueden generar varios eventos similares por segundo.
+    # Por ese motivo, si el evento no es necesario, es conveniente salir
+    # inmediatamente de la función para no bloquear la aplicación.
+        pass
+
+    elif evento == cv2.EVENT_LBUTTONDBLCLK:
+        print("Doble click botón izquierdo\n")
+        cv2.circle(img, (x,y), 3, color, -1)
+    elif evento == cv2.EVENT_LBUTTONDOWN:
+        coordenada_inicial = (x,y)
+        print("Pulsar botón izquierdo\n")
+    elif evento == cv2.EVENT_LBUTTONUP:
+        cv2.rectangle(img, coordenada_inicial, (x,y), (0,0,255), thickness)
+        print("Soltar botón izquierdo\n")
+    else:
+        print("Otro evento\n")
+
+    print("Coordenadas: x=", x, ", y=", y, "\n")
+    #Refrescamos la imagen en pantalla.
+    cv2.imshow("Imagen", img)
+
+
+
+#Abrimos la imagen indicada en el primer argumento
+try:
+    img=cv2.imread("pajaro.jpeg", cv2.IMREAD_UNCHANGED)
+except:
+    print("ERROR al abrir la imagen\n")
+
+#Comprobamos si la imagen se ha abierto correctamente.
+if img is None:
+    print("ERROR: Imagen: ", sys.argv[1], " no existe\n")
+else:
+    #Creamos una ventana donde mostrar la imagen.
+    cv2.namedWindow("Imagen", cv2.WINDOW_AUTOSIZE)
+    #Mostramos la imagen en pantalla.
+    cv2.imshow("Imagen", img)
+
+    #  Asociamos la función de manejo de eventos a esta ventana.
+    #  Pasamos la imagen como argumento a la función. Tener en
+    #  cuenta la conversión a puntero void para poder realizar
+    #  el paso. Observar en la callback como se deshace la
+    #  conversión.
+    color=(0,0,255)
+    cv2.setMouseCallback("Imagen", EventoRaton)
+    #Variable en la que capturaremos la tecla pulsada.
+    c=0
+    #Creamos un bucle infinito, que sólo finalizará al pulsar Esc.
+    while not c==27:
+        #Esperamos a que el usuario pulse una tecla.
+        c=cv2.waitKey(0) & 0xFF
+        if c == ord('g'):
+            color =(0,255,0)
+            print("Color cambiado a verde")
+        if c == ord('r'):
+            color =(0,0,255)
+            print("Color cambiado a rojo")
+        if c == ord('b'):
+            color =(255,0,0)    
+            print("Color cambiado a verde")
+        if c == ord('+'):
+            thickness +=1 
+            print("thickness aumentado a:",thickness)
+        if c == ord('-'):
+            if thickness==-1:
+                thickness = -1
+                print("thickness decrementado a:",thickness)
+            else:
+                thickness -=1    
+                print("thickness decrementado a:",thickness)    
+        #Imprimimos la tecla pulsada.
+        print(c,"\n")
+        
+
+    #Liberamos la ventana creada.
+    cv2.destroyWindow("Imagen")
+
