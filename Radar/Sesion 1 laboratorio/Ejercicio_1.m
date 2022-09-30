cf17d35408cdfd92b0254d98f16720fc5c7252dd
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-09-30 16:52:26 +0200

    Sesion radar

--- Radar/Sesion 1 laboratorio/Ejercicio_1.m
+++ Radar/Sesion 1 laboratorio/Ejercicio_1.m
@@ -6,8 +6,8 @@ T0=290; % K
 c=3e8; %m/s velocidad de la luz
 BW_senal=1e6;
 ancho_haz=1.2; %grados
-giro_antena=26; %rpm
-PRF=1950;%Hz
+giro_antena=26; %rpm "vueltas por minuto"
+PRF=1950;%Hz (es la frecuencia de muestreo )
 Rango_max_matriz=30*1000; %m;
 Cobertura_acimut= 120; %grados
 f_portadora=2.5e9; %Hz
@@ -67,19 +67,21 @@ ruido = sqrt(N0)*randn(n_celdas_rango,n_celdas_acimut) + 1i*sqrt(N0)*randn(n_cel
 
 eje_x = (1:n_celdas_rango)*Separacion_filas/1000;%eje distancias en km
 eje_y = (1:n_celdas_acimut)*Resolucion_en_distancia;%eje acimut en grados
-figure()
-
+figure();
 imagesc(eje_x,eje_y,20*log10(abs(ruido))); title('Ruido');
 xlabel('Distancia (km)');
 ylabel('Acimut (\phi)');
 
 [X,Y]=meshgrid(1:50,1:50);
-
 figure(); 
 mesh(X,Y, transpose(20*log10(abs(ruido(1:50,1:50)))));
-
 figure(); 
 mesh(X,Y, transpose((abs(ruido(1:50,1:50)))));
+ruido2 = randn(n_celdas_rango,n_celdas_acimut) *1i.*rand(n_celdas_rango,n_celdas_acimut)
+
+% Caractirazión estadistica del ruido
+% figure();hist(ruido2(:),200);title("Ruido2")
+
 
 datos = real(ruido(:));
 figure(), histogram(datos,50);
@@ -114,6 +116,7 @@ Resolucion_en_distancia)
 fila_blanco_estacionario=ceil(Acimut_blanco_estacionario/...
 Separacion_filas)
 ruido_blancos=ruido;
+
 if rem(Numero_pulsos,2)==0
  ruido_blancos((fila_blanco_estacionario-(Numero_pulsos/2)):...
 (fila_blanco_estacionario+(Numero_pulsos/2)-1),...
@@ -133,6 +136,91 @@ figure(); imagesc(eje_x,eje_y,20*log10(abs(ruido_blancos)));
 title('Blanco estacionario');
 
 
+constante=Er_estacionario*R_blanco_estacionario^4;
+
+Er_blanco_8km=constante/(8000^4);
+
+vector_blanco_8km=sqrt(2*Er_blanco_8km)*exp(1i*pi/5)*ones(Numero_pulsos,1);
+
+% La fila es la misma
+
+columna_blanco_8km=ceil(8000/Resolucion_en_distancia)
+
+if rem(Numero_pulsos,2)==0
+
+    ruido_blancos((fila_blanco_estacionario-(n_pulsos/2)):(fila_blanco_estacionario+(n_pulsos/2)-1),columna_blanco_8km)=ruido_blancos((fila_blanco_estacionario-(n_pulsos/2)):(fila_blanco_estacionario+(n_pulsos/2)-1),columna_blanco_8km)+vector_blanco_8km;
+
+else
+
+    ruido_blancos((fila_blanco_estacionario-fix(Numero_pulsos/2)):(fila_blanco_estacionario+fix(Numero_pulsos/2)),columna_blanco_8km)=ruido_blancos((fila_blanco_estacionario-fix(Numero_pulsos/2)):(fila_blanco_estacionario+fix(Numero_pulsos/2)),columna_blanco_8km)+vector_blanco_8km;
+
+end
+
+figure(); imagesc(eje_x,eje_y,20*log10(abs(ruido_blancos)));title('Blanco estacionario 8Km')
+
+close all;
+
+% Blanco a 7 Km
+constante=Er_estacionario*R_blanco_estacionario^4;
+
+Er_blanco_7km=constante/(7000^4);
+
+vector_blanco_7km=sqrt(2*Er_blanco_7km)*exp(1i*pi/5)*ones(Numero_pulsos,1);
+
+% La fila es la misma
+
+columna_blanco_7km=ceil(7000/Resolucion_en_distancia)
+
+if rem(Numero_pulsos,2)==0
+
+    ruido_blancos((fila_blanco_estacionario-(n_pulsos/2)):(fila_blanco_estacionario+(n_pulsos/2)-1),columna_blanco_7km)=ruido_blancos((fila_blanco_estacionario-(n_pulsos/2)):(fila_blanco_estacionario+(n_pulsos/2)-1),columna_blanco_7km)+vector_blanco_7km;
+
+else
+
+    ruido_blancos((fila_blanco_estacionario-fix(Numero_pulsos/2)):(fila_blanco_estacionario+fix(Numero_pulsos/2)),columna_blanco_7km)=ruido_blancos((fila_blanco_estacionario-fix(Numero_pulsos/2)):(fila_blanco_estacionario+fix(Numero_pulsos/2)),columna_blanco_7km)+vector_blanco_7km;
+
+end
+
+figure(); imagesc(eje_x,eje_y,20*log10(abs(ruido_blancos)));title('Blanco estacionario 7Km')
+
+% Cuanto se desplaza en 2.3s?
+Vrblanco_Kmh = 150;
+Vrblanco_mps = Vrblanco_Kmh*1000/3600;
+
+Duracion_una_vuelta = (60*360)/(giro_antena*360);
+Distancia_recorrida_metros = Vrblanco_mps * Duracion_una_vuelta
 
 
+% Blanco con movimiento radial
 
+R_blanco_radial=7e3; %m
+
+Acimut_blanco_radial=Acimut_blanco_estacionario; %grados
+
+v_radial=150; %km/h
+
+Er_blanco_radial=constante/(R_blanco_radial)^4;
+
+mov_radial_blanco=v_radial*1000/3600*Tiempo_iluminacion
+
+columna_blanco_radial=ceil(R_blanco_radial/Resolucion_en_distancia)
+
+fila_blanco_radial=fila_blanco_estacionario
+
+omega_Doppler=4*v_radial*1000/3600*pi/lambda
+
+vector_blanco_radial=sqrt(2*Er_blanco_radial)*exp(1i*pi/5)*exp(-1i*omega_Doppler/PRF*(0:(Numero_pulsos-1))).';
+
+SNR_blanco_radial=10*log10(Er_blanco_radial/N0)
+
+if rem(Numero_pulsos,2)==0
+
+    ruido_blancos((fila_blanco_radial-(Numero_pulsos/2)):(fila_blanco_radial+(Numero_pulsos/2)-1),columna_blanco_radial)=ruido_blancos((fila_blanco_radial-(Numero_pulsos/2)):(fila_blanco_radial+(Numero_pulsos/2)-1),columna_blanco_radial)+vector_blanco_radial;
+
+else
+
+    ruido_blancos((fila_blanco_radial-fix(Numero_pulsos/2)):(fila_blanco_radial+fix(Numero_pulsos/2)),columna_blanco_radial)=ruido_blancos((fila_blanco_radial-fix(Numero_pulsos/2)):(fila_blanco_radial+fix(Numero_pulsos/2)),columna_blanco_radial)+vector_blanco_radial;
+
+end
+
+figure(); imagesc((1:num_columnas)*R_resolucion/1000,(1:num_filas)*separacion_filas,20*log10(abs(ruido_blancos))); title('Blanco estacionario+giro+radial')
