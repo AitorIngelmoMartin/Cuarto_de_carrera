c2f9603d504a214ed2e8b24cb6e34814baf93baf
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-10-04 17:09:56 +0200

    Sesion de laboratorio radio 04/10

--- Radar/Sesion 1 laboratorio/blancos_matriz.m
+++ Radar/Sesion 1 laboratorio/blancos_matriz.m
@@ -2,12 +2,12 @@ function[matriz_con_blancos]=blancos_matriz(Er,theta_R,vr,fila,columna,n_pulsos,
 
 lambda=3e8/frecuencia;
 
+matriz_con_blancos=matriz_inicial;
+
 for target=1:length(Er)
 
-    matriz_con_blancos=matriz_inicial;
-
     if vr(target)==0
-        vector_blanco=sqrt(2*Er)*exp(1i*theta_R)*ones(n_pulsos,1);
+        vector_blanco=sqrt(2*Er(target))*exp(1i*theta_R(target))*ones(n_pulsos,1);
     else
         omega_Doppler=4*vr(target)*1000/3600*pi/lambda;
         vector_blanco=sqrt(2*Er(target))*exp(1i*pi/5)*exp(-1i*omega_Doppler/PRF*(0:(n_pulsos-1))).';
