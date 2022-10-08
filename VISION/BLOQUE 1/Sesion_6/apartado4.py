da611c9ae56ca7d526ae8dc0a8820f6cd0b52547
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-10-08 18:50:24 +0200

    Ejercicio 4 sesion 6

--- VISION/BLOQUE 1/Sesion_6/apartado4.py
+++ VISION/BLOQUE 1/Sesion_6/apartado4.py
@@ -1,4 +1,3 @@
-
 # import the necessary packages
 from imutils import contours
 from skimage import measure
@@ -12,22 +11,20 @@ try:
 except:
     print("ERROR al abrir la imagen\n")
 
-
-
 # Convert the image to grayscale, blur it and threshold
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray    = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (11, 11), 0)
-thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+thresh  = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+
 
 # perform a series of erosions and dilations to remove
 # any small blobs of noise from the thresholded image
 thresh = cv2.erode(thresh, None, iterations=2)
-thresh = cv2.dilate(thresh, None, iterations=4)
+thresh = cv2.dilate(thresh, None, iterations=8)
 
 #Connected component analysis
 labels = measure.label(thresh, background=0)
-mask = np.zeros(thresh.shape, dtype="uint8")
-
+mask   = np.zeros(thresh.shape, dtype="uint8")
 
 # loop over the unique components
 for label in np.unique(labels):
@@ -39,7 +36,7 @@ for label in np.unique(labels):
 	 # number of pixels 
      labelMask = np.zeros(thresh.shape, dtype="uint8")
      labelMask[labels == label] = 255
-     numPixels=cv2.countNonZero(labelMask)
+     numPixels = cv2.countNonZero(labelMask)
      # if the number of pixels in the component is sufficiently
 	 # large, then add it to our mask of "large blobs"
      print('label:%d, numPixels:%d\n'%(label,numPixels))
@@ -54,22 +51,19 @@ cv2.imshow("Mask", mask)
 
 # find the contours in the mask, then sort them from left to
 # right
-cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,	cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
-
 cnts = contours.sort_contours(cnts, method="left-to-right")[0]
 
 # Draw results 
 # loop over the contours
 for (i, c) in enumerate(cnts):
 	# draw the bright spot on the image
-	(x, y, w, h) = cv2.boundingRect(c)
+	(x, y, w, h)       = cv2.boundingRect(c)
 	((cX, cY), radius) = cv2.minEnclosingCircle(c)
-	cv2.circle(image, (int(cX), int(cY)), int(radius),
-		(0, 0, 255), 3)
-	cv2.putText(image, "#{}".format(i + 1), (x, y - 15),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255),1)
+
+	cv2.circle(image, (int(cX), int(cY)), int(radius), (0, 0, 255), 3)
+	cv2.putText(image, "#{}".format(i + 1), (x, y - 15),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255),1)
 
 # show the output image
 cv2.imshow("Image", image)
