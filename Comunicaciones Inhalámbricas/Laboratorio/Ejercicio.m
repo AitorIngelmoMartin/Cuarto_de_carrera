b1db7163e2bb45e41d4909f9c2174e4b1c560b2a
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-10-10 12:11:08 +0200

    Fin ejercicio 1 inhalámbricas

--- Comunicaciones Inhalámbricas/Laboratorio/Ejercicio.m
+++ Comunicaciones Inhalámbricas/Laboratorio/Ejercicio.m
@@ -15,25 +15,34 @@ Alfa  = [2.31, 3.79];
 Beta  = [24.52, 21.01];
 Gamma = [2.06, 1.34];
 Sigma = [2.69, 9.05];
-% Prx a 500 metros
 
-
+% Apartado 1:Potencia media recibida a 500m del punto de acceso
 Distancia = 500;
+
 Lb_dB   = 10*Alfa*log10(Distancia) + Beta + 10*Gamma*log10(f);
 Prx_dBm = Ptx_dBm + Gtx_dB  - Lb_dB + Grx_dB;
 
-probabilidad = [0.9];
+% Apartado 2: Potencia recibida superada en el 90% de los puntos situados 
+% a 500m del punto de acceso
+probabilidad = 0.9;
+
 Aux    = erfcinv(2*probabilidad);
 Prx_90 = Aux*sqrt(2)*Sigma + Prx_dBm;
 
-% Apartado 3:
-Porcentaje = erfc(((-85-Prx_dBm))./(sqrt(2).*Sigma))./2;
+% Apartado 3: Porcentaje de puntos situados a 500m del punto de acceso que
+% reciben una potencia mayor a -85dBm
+Porcentaje_Ptx = erfc(((-85-Prx_dBm))./(sqrt(2).*Sigma))./2;
 
 % Apartado 4: Radio de cobertura (Rcob1) con una calidad del 50%
-Lb_2_dB = -U_dBm + Ptx_dBm + Gtx_dB + Grx_dB;
-
-
-R_cob1_50 = 10.^((Lb_2_dB - Beta - 10*Gamma*log10(f))./(10*Alfa));
+Lb_2_dB         = -U_dBm + Ptx_dBm + Gtx_dB + Grx_dB;
+R_cobertura1_50 = 10.^((Lb_2_dB - Beta - 10*Gamma*log10(f))./(10*Alfa));
 
 % Apartado 5: Radio de cobertura (Rcob1) con una calidad del 90%
-Porcentaje = erfc(((-85-Prx_dBm))./(sqrt(2).*Sigma))./2;
+Aux    = U_dBm-erfcinv(2*0.9)*sqrt(2)*Sigma;
+Lb2_dB = Ptx_dBm+Gtx_dB+Grx_dB-Aux;
+
+R_cobertura1_90 = 10.^((Lb2_dB-Beta-10*Gamma*log10(f))./(10*Alfa))*1e-1;%En Km
+
+%  Apartado 6: Determinar la potencia que había que transmitir para
+% conseguir un radio de cobertura(Rcob1) con una calidad del 90%
+Ptx_dBm_R_cobertura1_90=Aux-Gtx_dB-Grx_dB+Lb_2_dB;
