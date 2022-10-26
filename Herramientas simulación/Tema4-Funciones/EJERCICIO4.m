b5c88dd2f290421e47b5b3fb06677ba2c5a4464c
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-10-26 16:56:34 +0200

    Fin funciones Herramientas
    Queda implementar las funciones como tal, pero los algoritmos están hechos.

--- Herramientas simulación/Tema4-Funciones/EJERCICIO4.m
+++ Herramientas simulación/Tema4-Funciones/EJERCICIO4.m
@@ -1,17 +1,5 @@
 clc;clear;
 
-% Escriba una función myorder que ordene un vector de escalares. 
-% Esta función tendrá como mínimo un parámetro  de entrada que será el vector. 
-% 
-% Si se llama a la función con un único parámetro de entrada la función recorrerá
-% el vector y ordenará sus elementos de mayor a menor. Si se llama a la función con 
-% dos parámetros de entrada el segundo argumento aceptará una cadena de caracteres 
-% cuyo valor puede ser 'ascendente' o 'descendente',
-% y en función de este valor ordenará el vector de menor a mayor o de mayor a menor, 
-% respectivamente. En ambos casos la función debe devolver un vector ordenado. 
-% 
-% Use la función sort dentro de su función.
-
 tipo = "ascendente";
 vector = [-9,57,50-7,0,6,14,3];
 
--- Herramientas simulación/Tema4-Funciones/mux.m
+++ Herramientas simulación/Tema4-Funciones/mux.m
@@ -1,18 +1,45 @@
-clc;clear;
-
-% Escriba una función que tenga como entrada dos valores y devuelva 
-% una matriz cuadrada. El primer argumento de entrada indicará el tamaño 
-% de la matriz. El segundo argumento x, servirá para calcular los elementos
-% de la matriz tal que: 
-
-orden = 5;
+% clc;clear;
+% 
+% % Escriba una función que tenga como entrada dos valores y devuelva 
+% % una matriz cuadrada. El primer argumento de entrada indicará el tamaño 
+% % de la matriz. El segundo argumento x, servirá para calcular los elementos
+% % de la matriz tal que: 
+% 
+% orden = 5;
 x = 9
-matriz = zeros(orden);
+% matriz = zeros(orden);
+% 
+% i=1:size(matriz,1);
+% vector(i) = x.^(i-1);
+% for i=1:size(matriz,1)
+%     matriz(i,:)=vector
+% end
+% matriz_prueba = [1,2,3;2,3,0;3,0,0]
+% 
+% 
+% diag([1,0,0;1,0,0;1,0,0])
+% 
+
+
+clc;clear;
+valor  = 6;
+matriz = zeros(valor);
 
 i=1:size(matriz,1);
-vector(i) = x.^(i-1);
-for i=1:size(matriz,1)
-    matriz(i,:)=vector
+vector = valor.^(i-1);
+
+
+for i=1:(2*size(matriz,1)-1)
+    if i <=valor
+       vector(i) = valor^(i-1)
+    else
+       vector(i) = vector(i-1)/valor
+    end
 end
+
+for i=1:size(matriz,1)
+    matriz(i,:)=vector(i:(i+(valor-1)))
+end
+
 
 
