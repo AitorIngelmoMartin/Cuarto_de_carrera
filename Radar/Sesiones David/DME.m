1de05b56c063a038d77d2d86d43a3fe5104b2df1
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-12-18 13:21:22 +0100

    Practica 12 visión

--- Radar/Sesiones David/DME.m
+++ Radar/Sesiones David/DME.m
@@ -57,16 +57,16 @@ plot(t,(pulso1+pulso2))
 
 % La idea de mandar un par de pulsos nos sirve para identificar la pregunta
 % que estoy haciendo.
-
+% 
 % La cosa es que no vamos a mandar 1 pregunta, sino un tren de ellas. La
 % idea es que la separación entre preguntas sea aleatoria. Esta secuencia
 % aleatoria SOLO LA CONOCE el avión. De hecho esta es la forma que tiene el
 % avión para saber que es para él.
-
+% 
 % Estos espacios entre pulsos X1-X2 tienen un margen. Si es de 120-150 es
 % modo búsqueda. Ya que nos interesa saber donde está. No es un valor fijo,
 % alterna entre este valor.
-
+% 
 % El siguiente paso es seguimiento, hacemos [24-30] preguntas por segundo.
 
 
@@ -137,16 +137,111 @@ distancia_que_ver = [Distancia, 2*Distancia, 3*Distancia, 4*Distancia]
 
 %%
 
-Preguntas hechas
-
-recibidas transpondedor
-50u6
-generadas por él
-
-recibidas por avión.
+% Preguntas hechas
+% 
+% recibidas transpondedor
+% 50u6
+% generadas por él
+% 
+% recibidas por avión.
 
 
 % Al tren de preguntas hay que añadir el viaje de ida, el retardo
 % transpondedor, y el viaje vuelta.
 
 % DME a 110 Km
+
+%% TREN DE PREGUNTAS
+
+frf = 1000;
+fs  = 32e6;
+sigma      = 3.5/(2*sqrt(2*log(2)));
+
+
+% Añado el tiempo total que necesito para considerar los retardos
+Distancia_avion_transpondedor = 110; % Km
+Retardo_avion_transpondedor   = (Distancia_avion_transpondedor*1E9/3E8);
+
+tiempo_silencio  = 50;
+Tiempo_adicional = 2*Retardo_avion_transpondedor + tiempo_silencio;
+
+
+t = -5:((10^6)/fs):(4120+Tiempo_adicional);
+
+t0         = [0, 1500, 2900, 4100]
+t0_segundo = t0 +12;
+tren_preguntas =0;
+
+figure()
+hold on
+
+for i=1:size(t0,2)    
+    envolvente = exp(-(t-t0(i)).^2/(2*sigma^2));
+    
+    pulso1     = envolvente.*cos(2*pi*frf*t);      
+    
+    envolvente = exp(-(t-t0_segundo(i)).^2/(2*sigma^2));
+
+    pulso2 = envolvente.*cos(2*pi*frf*t);
+    tren_preguntas  = tren_preguntas+ pulso1 + pulso2;
+end
+
+plot(t,tren_preguntas)
+
+%% TREN DE PREGUNTAS EN TRANSPONDEDOR
+
+tren_preguntas_retardoTx1 = 0;
+for i=1:size(t0,2)    
+    envolvente = exp(-(t-(t0(i)+Retardo_avion_transpondedor)).^2/(2*sigma^2));
+    
+    pulso1     = envolvente.*cos(2*pi*frf*t);      
+    
+    envolvente = exp(-(t-(t0_segundo(i)+Retardo_avion_transpondedor)).^2/(2*sigma^2));
+
+    pulso2 = envolvente.*cos(2*pi*frf*t);
+    tren_preguntas_retardoTx1  = tren_preguntas_retardoTx1+ pulso1 + pulso2;
+end
+
+
+
+plot(t,tren_preguntas_retardoTx1)
+
+%% TREN DE PREGUNTAS TRAS LOS 50US DE SILENCIO
+
+tren_preguntas_tras_silencio = 0;
+for i=1:size(t0,2)    
+    envolvente = exp(-(t-(t0(i)+Retardo_avion_transpondedor + tiempo_silencio)).^2 ...
+        /(2*sigma^2));
+    
+    pulso1     = envolvente.*cos(2*pi*frf*t);      
+    
+    envolvente = exp(-(t-(t0_segundo(i)+Retardo_avion_transpondedor + tiempo_silencio)).^2 ...
+        /(2*sigma^2));
+
+    pulso2 = envolvente.*cos(2*pi*frf*t);
+    tren_preguntas_tras_silencio  = tren_preguntas_tras_silencio+ pulso1 + pulso2;
+end
+
+
+plot(t,tren_preguntas_tras_silencio)
+
+%% TREN DE PREGUNTAS TRAS TX2
+
+tren_preguntas_retardoTx2 = 0;
+for i=1:size(t0,2)    
+    envolvente = exp(-(t-(t0(i)+2*Retardo_avion_transpondedor + tiempo_silencio)).^2 ...
+        /(2*sigma^2));
+    
+    pulso1     = envolvente.*cos(2*pi*frf*t);      
+    
+    envolvente = exp(-(t-(t0_segundo(i)+2*Retardo_avion_transpondedor + tiempo_silencio)).^2 ...
+        /(2*sigma^2));
+
+    pulso2 = envolvente.*cos(2*pi*frf*t);
+    tren_preguntas_retardoTx2  = tren_preguntas_retardoTx2+ pulso1 + pulso2;
+end
+
+
+plot(t,tren_preguntas_retardoTx2)
+legend("Tren preguntas original","Tras retardo de Tx1","Tras tiempo de silencio","Tras retardo de Tx2");
+title("Tren de preguntas en un caso real");
